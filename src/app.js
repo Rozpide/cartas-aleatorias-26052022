8720bdd3cd8e0789eb5f39fb555d1b3158276722
Rozpi <joserozpide@yahoo.es>
2022-05-26 10:07:33 +0000

    cartas-260522

--- src/app.js
+++ src/app.js
@@ -7,5 +7,35 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  document.querySelector("#boton").addEventListener("click", function() {
+    let valor = [
+      "1",
+      "2",
+      "3",
+      "4",
+      "5",
+      "6",
+      "7",
+      "8",
+      "9",
+      "10",
+      "J",
+      "Q",
+      "K"
+    ];
+    document.querySelector(".Reemplazar").innerHTML =
+      valor[RandomCards(0, valor.length)];
+    console.log(valor[RandomCards(0, valor.length - 1)]);
+    Probando();
+    console.log(card[RandomCards(0, card.length - 1)]);
+  });
+  let card = ["Pica", "Trebol", "Corazones", "Diamante"];
+
+  function RandomCards(min, max) {
+    return Math.floor(Math.random() * (max - min) + min);
+  }
+  function Probando() {
+    document.querySelector(".valor").className =
+      " valor " + card[RandomCards(0, card.length)];
+  }
 };
